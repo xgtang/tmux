5c927e350cee7e3714bb4658abcc1d9f8fc2e21b
apao <apao@mail.com>
2021-07-01 20:00:22 +0800

    update

--- with-proxy.sh
+++ with-proxy.sh
@@ -11,11 +11,13 @@ export SOCKS_PROXY="127.0.0.1:1086"
 export http_proxy="http://127.0.0.1:1087"
 export https_proxy="http://127.0.0.1:1087"
 export SSH_PROXY_FILE="${HOME}/.ssh/config"
+export SSH_PROXY_FILE_BAK="${HOME}/.ssh/config_bak"
+
+mv -f $SSH_PROXY_FILE $SSH_PROXY_FILE_BAK
 
 gen_ssh_proxy_files
 function finish {
-   rm -f $SSH_PROXY_FILE
+   mv -f $SSH_PROXY_FILE_BAK $SSH_PROXY_FILE
 }
 trap finish EXIT
-
 $@
